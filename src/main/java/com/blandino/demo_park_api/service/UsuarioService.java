488763cf83764f78419d2813a4d2b87c104651b8
Mucumbe <mucumbeblandino@gmail.com>
2026-02-03 16:51:49 +0200

    Recursos para add novo usuario

--- src/main/java/com/blandino/demo_park_api/service/UsuarioService.java
+++ src/main/java/com/blandino/demo_park_api/service/UsuarioService.java
@@ -1,6 +1,8 @@
 package com.blandino.demo_park_api.service;
 
+import com.blandino.demo_park_api.entity.Usuario;
 import com.blandino.demo_park_api.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +12,9 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
 
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+
+        return usuarioRepository.save(usuario);
+    }
 }
