18646bd98ef61cb38190718efa13f605699eee87
Mucumbe <mucumbeblandino@gmail.com>
2026-02-03 18:45:23 +0200

    Melhorias no Recurso Usuario

--- src/main/java/com/blandino/demo_park_api/service/UsuarioService.java
+++ src/main/java/com/blandino/demo_park_api/service/UsuarioService.java
@@ -2,9 +2,9 @@ package com.blandino.demo_park_api.service;
 
 import com.blandino.demo_park_api.entity.Usuario;
 import com.blandino.demo_park_api.repository.UsuarioRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,5 +16,13 @@ public class UsuarioService {
     public Usuario salvar(Usuario usuario) {
 
         return usuarioRepository.save(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario buscarPorId(Long id) {
+
+        return usuarioRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuario não encontrado")
+        );
     }
 }
--- src/main/java/com/blandino/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/blandino/demo_park_api/web/controller/UsuarioController.java
@@ -5,10 +5,7 @@ import com.blandino.demo_park_api.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +19,12 @@ public class UsuarioController {
 
         Usuario user=usuarioService.salvar(usuario);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping("/{id}")
+    public  ResponseEntity<Usuario> findById(@PathVariable Long id){
+        Usuario usuario=usuarioService.buscarPorId(id);
+        return ResponseEntity.ok(usuario);
     }
 
 }
