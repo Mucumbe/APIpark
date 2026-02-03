59fe18e628d487bcb695c4666bf42d64e8ac12bc
Mucumbe <mucumbeblandino@gmail.com>
2026-02-03 16:36:21 +0200

    Crinado As classes Service, repository e controller do usuario

---
+++ src/main/java/com/blandino/demo_park_api/service/UsuarioService.java
@@ -0,0 +1,13 @@
+package com.blandino.demo_park_api.service;
+
+import com.blandino.demo_park_api.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+}
---
+++ src/main/java/com/blandino/demo_park_api/web/controller/UsuarioController.java
@@ -0,0 +1,15 @@
+package com.blandino.demo_park_api.web.controller;
+
+import com.blandino.demo_park_api.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+}
