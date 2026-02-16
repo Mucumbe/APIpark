671adaf8fd63f30aee1a2d83a15397a4c30d6919
Mucumbe <mucumbeblandino@gmail.com>
2026-02-16 13:00:16 +0200

    Criando objecto de resposta Token

---
+++ src/main/java/com/blandino/demo_park_api/jwt/JwtToken.java
@@ -0,0 +1,17 @@
+package com.blandino.demo_park_api.jwt;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtToken {
+
+    private String token;
+
+}
