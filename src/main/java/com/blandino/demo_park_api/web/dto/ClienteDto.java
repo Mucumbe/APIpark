0ed8639455182e9d573a4de08ea5934292959bbd
Mucumbe <mucumbeblandino@gmail.com>
2026-06-05 20:22:32 +0200

    Criacao de DTO de cliente e o Mapper

---
+++ src/main/java/com/blandino/demo_park_api/web/dto/ClienteDto.java
@@ -0,0 +1,22 @@
+package com.blandino.demo_park_api.web.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClienteDto {
+
+    @NotNull
+    @Size(min = 5,max = 50)
+    private String nome;
+
+    @Size(min =5 ,max =9 )
+    private String nuit;
+
+}
---
+++ src/main/java/com/blandino/demo_park_api/web/dto/mapper/ClienteMapper.java
@@ -0,0 +1,23 @@
+package com.blandino.demo_park_api.web.dto.mapper;
+
+
+import com.blandino.demo_park_api.entity.Cliente;
+import com.blandino.demo_park_api.web.dto.ClienteDto;
+import com.blandino.demo_park_api.web.dto.ClienteResponseDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ClienteMapper {
+
+    //Converte Um dto Simples para um Usuario Mas complexo
+    public static Cliente toCliente(ClienteDto dto){
+        return  new ModelMapper().map(dto,Cliente.class);
+    }
+
+    public  static ClienteResponseDto toDto(Cliente cliente){
+        return new ModelMapper().map(cliente,ClienteResponseDto.class);
+    }
+
+}
