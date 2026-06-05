c413a9fac9a058695cc5c62ed6648a373a32c5ba
Mucumbe <mucumbeblandino@gmail.com>
2026-06-05 18:53:36 +0200

    Adicionando recursos de teste de usuarario logado com sucesso na aplicacao 200

---
+++ src/test/java/com/blandino/demo_park_api/AutenticacaoIT.java
@@ -0,0 +1,50 @@
+package com.blandino.demo_park_api;
+
+import com.blandino.demo_park_api.jwt.JwtToken;
+import com.blandino.demo_park_api.web.dto.UsuarioLoginDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/usuarios/usuarios-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/usuarios/usuarios-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class AutenticacaoIT {
+
+
+    WebTestClient testClient;
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void SetUp() {
+        this.testClient = WebTestClient
+                .bindToServer()
+                .baseUrl("http://localhost:" + port)
+                .build();
+    }
+
+    @Test
+    public void createUser_with_validCredencial() {
+
+        JwtToken jwtToken = testClient
+                .post()
+                .uri("/api/v1/auth")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioLoginDto("boas@kk.co", "123456789"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(JwtToken.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(jwtToken).isNotNull();
+
+    }
+}
