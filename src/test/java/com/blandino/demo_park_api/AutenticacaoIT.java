eae28fc1605fe82ef3f61079e9ef74f95f50e198
Mucumbe <mucumbeblandino@gmail.com>
2026-06-05 19:11:53 +0200

    Adicionando recursos de teste de usuarario com credenciais invalidas 400

--- src/test/java/com/blandino/demo_park_api/AutenticacaoIT.java
+++ src/test/java/com/blandino/demo_park_api/AutenticacaoIT.java
@@ -2,11 +2,14 @@ package com.blandino.demo_park_api;
 
 import com.blandino.demo_park_api.jwt.JwtToken;
 import com.blandino.demo_park_api.web.dto.UsuarioLoginDto;
+import com.blandino.demo_park_api.web.exception.ErrorMessage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -45,6 +48,25 @@ public class AutenticacaoIT {
                 .returnResult().getResponseBody();
 
         org.assertj.core.api.Assertions.assertThat(jwtToken).isNotNull();
+
+    }
+
+    @Test
+    public void createUser_with_notvalidCredencial400() {
+
+        ErrorMessage jwtToken = testClient
+                .post()
+                .uri("/api/v1/auth")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioLoginDto("boas@kk.co", "1234567890"))
+                .exchange()
+                .expectStatus().isEqualTo(400)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(jwtToken).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(jwtToken.getStatus()).isEqualTo(400);
+
 
     }
 }
