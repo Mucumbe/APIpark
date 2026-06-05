7331591c5809697c0139262a31e0a8d5fb83214b
Mucumbe <mucumbeblandino@gmail.com>
2026-06-05 19:45:56 +0200

    Criacao de entidade Cliente

---
+++ src/main/java/com/blandino/demo_park_api/entity/Cliente.java
@@ -0,0 +1,52 @@
+package com.blandino.demo_park_api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+@Entity
+@Table(name = "clientes")
+@EntityListeners(AuditingEntityListener.class)
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome",nullable = false,length = 50)
+    private String nome;
+
+    @Column(name = "nuit",nullable = false,length = 9)
+    private String nuit ;
+
+    @OneToOne
+    @Column(name = "is_usuario", nullable = false)
+    private  Usuario usuario;
+
+    @CreatedDate
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+
+    @LastModifiedDate
+    @Column(name = "data_modificacao")
+    private LocalDateTime dataModificacao;
+
+    @CreatedBy
+    @Column(name = "criado_por")
+    private String criadoPor;
+
+    @LastModifiedBy
+    @Column(name = "modificado_por")
+    private String modificadoPor;
+
+}
