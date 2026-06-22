febf39334ea9cd784ba16eec450483702a560105
Mucumbe <mucumbeblandino@gmail.com>
2026-06-22 22:09:00 +0200

    criado a class controller com metodos de criacao e pesquisa de vaga

---
+++ src/main/java/com/blandino/demo_park_api/web/controller/VagaController.java
@@ -0,0 +1,41 @@
+package com.blandino.demo_park_api.web.controller;
+
+import com.blandino.demo_park_api.entity.Vaga;
+import com.blandino.demo_park_api.service.VagaService;
+import com.blandino.demo_park_api.web.dto.VagaCreateDto;
+import com.blandino.demo_park_api.web.dto.VagaResponseDto;
+import com.blandino.demo_park_api.web.dto.mapper.VagaMapper;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/v1/vagas")
+public class VagaController {
+
+
+    private final VagaService service;
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> create(@RequestBody @Valid VagaCreateDto dto) {
+        Vaga vaga = VagaMapper.toVaga(dto);
+        vaga = service.salvar(vaga);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequestUri().
+                path("/{codigo}").buildAndExpand(vaga.getCodigo()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("/{codigo}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<VagaResponseDto> getBycodigo(@PathVariable String codigo){
+        Vaga vaga= service.buscarPorCodigo(codigo);
+        return  ResponseEntity.ok(VagaMapper.toDto(vaga));
+    }
+}
